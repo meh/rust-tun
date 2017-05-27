3980cab67839e91de6cf03047b74540791610082
meh <meh@schizofreni.co>
2017-05-27 16:33:07 +0200

    configuration: refactor builder, takes a mutable reference now

--- src/configuration.rs
+++ src/configuration.rs
@@ -17,48 +17,55 @@ use device::Device;
 use address::IntoAddress;
 
 pub struct Configuration<T: Device> {
-	inner: T,
+	inner: Option<T>,
 }
 
 impl<T: Device> Configuration<T> {
 	pub fn new(inner: T) -> Self {
 		Configuration {
-			inner: inner
+			inner: Some(inner)
 		}
 	}
 
-	pub fn name<S: AsRef<str>>(mut self, name: S) -> Result<Self> {
-		self.inner.set_name(name.as_ref())?;
+	pub fn name<S: AsRef<str>>(&mut self, name: S) -> Result<&mut Self> {
+		self.inner.as_mut().unwrap().set_name(name.as_ref())?;
 		Ok(self)
 	}
 
-	pub fn address<A: IntoAddress>(mut self, value: A) -> Result<Self> {
-		self.inner.set_address(value.into_address()?)?;
+	pub fn address<A: IntoAddress>(&mut self, value: A) -> Result<&mut Self> {
+		self.inner.as_mut().unwrap().set_address(value.into_address()?)?;
 		Ok(self)
 	}
 
-	pub fn destination<A: IntoAddress>(mut self, value: A) -> Result<Self> {
-		self.inner.set_destination(value.into_address()?)?;
+	pub fn destination<A: IntoAddress>(&mut self, value: A) -> Result<&mut Self> {
+		self.inner.as_mut().unwrap().set_destination(value.into_address()?)?;
 		Ok(self)
 	}
 
-	pub fn broadcast<A: IntoAddress>(mut self, value: A) -> Result<Self> {
-		self.inner.set_broadcast(value.into_address()?)?;
+	pub fn broadcast<A: IntoAddress>(&mut self, value: A) -> Result<&mut Self> {
+		self.inner.as_mut().unwrap().set_broadcast(value.into_address()?)?;
 		Ok(self)
 	}
 
-	pub fn netmask<A: IntoAddress>(mut self, value: A) -> Result<Self> {
-		self.inner.set_netmask(value.into_address()?)?;
+	pub fn netmask<A: IntoAddress>(&mut self, value: A) -> Result<&mut Self> {
+		self.inner.as_mut().unwrap().set_netmask(value.into_address()?)?;
 		Ok(self)
 	}
 
-	pub fn up(mut self) -> Result<T> {
-		self.inner.enabled(true)?;
-		Ok(self.inner)
+	pub fn device<F>(&mut self, f: F) -> Result<&mut Self>
+		where F: FnOnce(&mut T) -> Result<()>
+	{
+		f(self.inner.as_mut().unwrap())?;
+		Ok(self)
 	}
 
-	pub fn down(mut self) -> Result<T> {
-		self.inner.enabled(false)?;
-		Ok(self.inner)
+	pub fn up(&mut self) -> Result<T> {
+		self.inner.as_mut().unwrap().enabled(true)?;
+		Ok(self.inner.take().unwrap())
+	}
+
+	pub fn down(&mut self) -> Result<T> {
+		self.inner.as_mut().unwrap().enabled(false)?;
+		Ok(self.inner.take().unwrap())
 	}
 }
