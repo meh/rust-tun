c64350decec8c0f634927bfbb10f5c288ada5b8c
xmh0511 <970252187@qq.com>
2023-12-01 22:27:44 +0800

    optmize tun read/write performance for windows platform

--- src/async/win/device.rs
+++ src/async/win/device.rs
@@ -21,7 +21,7 @@ use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 use tokio_util::codec::Framed;
 
 use crate::device::Device as D;
-use crate::platform::Device;
+use crate::platform::{Device, Queue};
 use crate::r#async::codec::*;
 
 pub struct AsyncDevice {
@@ -56,9 +56,10 @@ impl AsyncRead for AsyncDevice {
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<io::Result<()>> {
-        //let rbuf = buf.initialize_unfilled();
-        match Pin::new(&mut self.inner).poll_read(cx, buf) {
-            Poll::Ready(Ok(_)) => {
+        let rbuf = buf.initialize_unfilled();
+        match Pin::new(&mut self.inner).poll_read(cx, rbuf) {
+            Poll::Ready(Ok(size)) => {
+                buf.advance(size);
                 Poll::Ready(Ok(()))
             }
             Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
@@ -91,69 +92,64 @@ impl AsyncWrite for AsyncDevice {
     }
 }
 
-pub struct AsyncQueue;
+pub struct AsyncQueue {
+    inner: Queue,
+}
 
-// pub struct AsyncQueue {
-//     inner: Queue,
-// }
+impl AsyncQueue {
+    /// Create a new `AsyncQueue` wrapping around a `Queue`.
+    pub fn new(queue: Queue) -> io::Result<AsyncQueue> {
+        Ok(AsyncQueue { inner: queue })
+    }
+    /// Returns a shared reference to the underlying Queue object
+    pub fn get_ref(&self) -> &Queue {
+        &self.inner
+    }
 
-// impl AsyncQueue {
-//     /// Create a new `AsyncQueue` wrapping around a `Queue`.
-//     pub fn new(queue: Queue) -> io::Result<AsyncQueue> {
-//         Ok(AsyncQueue { inner: queue })
-//     }
-//     /// Returns a shared reference to the underlying Queue object
-//     pub fn get_ref(&self) -> &Queue {
-//         &self.inner
-//     }
+    /// Returns a mutable reference to the underlying Queue object
+    pub fn get_mut(&mut self) -> &mut Queue {
+        &mut self.inner
+    }
 
-//     /// Returns a mutable reference to the underlying Queue object
-//     pub fn get_mut(&mut self) -> &mut Queue {
-//         &mut self.inner
-//     }
+    /// Consumes this AsyncQueue and return a Framed object (unified Stream and Sink interface)
+    pub fn into_framed(self) -> Framed<Self, TunPacketCodec> {
+        let codec = TunPacketCodec::new(false, 1512);
+        Framed::new(self, codec)
+    }
+}
 
-//     /// Consumes this AsyncQueue and return a Framed object (unified Stream and Sink interface)
-//     pub fn into_framed(self) -> Framed<Self, TunPacketCodec> {
-//         let codec = TunPacketCodec::new(false, 1512);
-//         Framed::new(self, codec)
-//     }
-// }
+impl AsyncRead for AsyncQueue {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        let rbuf = buf.initialize_unfilled();
+        match Pin::new(&mut self.inner).poll_read(cx, rbuf) {
+            Poll::Ready(Ok(_)) => Poll::Ready(Ok(())),
+            Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
 
-// impl AsyncRead for AsyncQueue {
-//     fn poll_read(
-//         mut self: Pin<&mut Self>,
-//         cx: &mut Context<'_>,
-//         buf: &mut ReadBuf<'_>,
-//     ) -> Poll<io::Result<()>> {
-//         let rbuf = buf.initialize_unfilled();
-//         match Pin::new(&mut self.inner).poll_read(cx, rbuf) {
-//             Poll::Ready(Ok(n)) => {
-//                 buf.advance(n);
-//                 Poll::Ready(Ok(()))
-//             }
-//             Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
-//             Poll::Pending => Poll::Pending,
-//         }
-//     }
-// }
+impl AsyncWrite for AsyncQueue {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, Error>> {
+        match self.inner.write(buf) {
+            Ok(n) => Poll::Ready(Ok(n)),
+            Err(e) => Poll::Ready(Err(e)),
+        }
+    }
 
-// impl AsyncWrite for AsyncQueue {
-//     fn poll_write(
-//         mut self: Pin<&mut Self>,
-//         _cx: &mut Context<'_>,
-//         buf: &[u8],
-//     ) -> Poll<Result<usize, Error>> {
-//         match self.inner.write(buf) {
-//             Ok(n) => Poll::Ready(Ok(n)),
-//             Err(e) => Poll::Ready(Err(e)),
-//         }
-//     }
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))
+    }
 
-//     fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-//         Poll::Ready(Ok(()))
-//     }
-
-//     fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-//         Poll::Ready(Ok(()))
-//     }
-// }
+    fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))
+    }
+}
--- src/platform/windows/device.rs
+++ src/platform/windows/device.rs
@@ -14,12 +14,12 @@
 
 use std::io::{self, Read, Write};
 use std::net::{IpAddr, Ipv4Addr};
+#[cfg(feature = "async")]
+use std::pin::Pin;
 use std::sync::Arc;
+#[cfg(feature = "async")]
 use std::thread;
-use std::vec::Vec;
 
-use bytes::BufMut;
-use tokio::io::ReadBuf;
 use wintun::Session;
 
 use crate::configuration::Configuration;
@@ -28,10 +28,34 @@ use crate::error::*;
 
 /// A TUN device using the wintun driver.
 pub struct Device {
-    session: Arc<Session>,
-    receiver: tokio::sync::mpsc::UnboundedReceiver<Vec<u8>>,
-    _task: thread::JoinHandle<()>,
-    mtu:usize
+    queue: Queue,
+    mtu: usize,
+}
+
+#[cfg(feature = "async")]
+fn create_queue(session: Session) -> Queue {
+    let session = Arc::new(session);
+    let (receiver_tx, receiver_rx) = tokio::sync::mpsc::unbounded_channel::<Vec<u8>>();
+    let session_reader = session.clone();
+    let task = thread::spawn(move || {
+        while let Ok(packet) = session_reader.receive_blocking() {
+            let bytes = packet.bytes().to_vec();
+            //dbg!(&bytes);
+            receiver_tx.send(bytes).unwrap();
+        }
+    });
+    Queue {
+        session,
+        receiver: receiver_rx,
+        _task: task,
+    }
+}
+
+#[cfg(not(feature = "async"))]
+fn create_queue(session: Session) -> Queue {
+    Queue {
+        session: Arc::new(session),
+    }
 }
 
 impl Device {
@@ -39,10 +63,9 @@ impl Device {
     pub fn new(config: &Configuration) -> Result<Self> {
         let wintun = unsafe { wintun::load()? };
         let tun_name = config.name.as_deref().unwrap_or("wintun");
-        let guid = Some(9099482345783245345345_u128);
         let adapter = match wintun::Adapter::open(&wintun, tun_name) {
             Ok(a) => a,
-            Err(_) => wintun::Adapter::create(&wintun, tun_name, tun_name, guid)?,
+            Err(_) => wintun::Adapter::create(&wintun, tun_name, tun_name, None)?,
         };
 
         let address = config.address.ok_or(Error::InvalidConfig)?;
@@ -51,25 +74,11 @@ impl Device {
         adapter.set_network_addresses_tuple(IpAddr::V4(address), IpAddr::V4(mask), gateway)?;
         let mtu = config.mtu.unwrap_or(1500) as usize;
 
-        let session = Arc::new(adapter.start_session(wintun::MAX_RING_CAPACITY)?);
-
-        let (receiver_tx, receiver_rx) = tokio::sync::mpsc::unbounded_channel::<Vec<u8>>();
-
-        let session_reader = session.clone();
-        let task = thread::spawn(move || {
-            loop {
-                let packet = session_reader.receive_blocking().unwrap();
-                let bytes = packet.bytes().to_vec();
-                // dbg!(&bytes);
-                receiver_tx.send(bytes).unwrap();
-            }
-        });
+        let session = adapter.start_session(wintun::MAX_RING_CAPACITY)?;
 
         let mut device = Device {
-            session,
-            receiver: receiver_rx,
-            _task: task,
-            mtu
+            queue: create_queue(session),
+            mtu,
         };
 
         // This is not needed since we use netsh to set the address.
@@ -77,18 +86,174 @@ impl Device {
 
         Ok(device)
     }
+}
 
+#[cfg(feature = "async")]
+impl Device {
     pub fn poll_read(
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
-        buf: &mut ReadBuf<'_>,
-    ) -> std::task::Poll<std::io::Result<()>> {
+        buf: &mut [u8],
+    ) -> std::task::Poll<std::io::Result<usize>> {
+        Pin::new(&mut self.queue).poll_read(cx, buf)
+    }
+
+    pub fn poll_write(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> std::task::Poll<Result<usize, std::io::Error>> {
+        Pin::new(&mut self.queue).poll_write(cx, buf)
+    }
+
+    pub fn poll_flush(
+        self: std::pin::Pin<&mut Self>,
+        _cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        std::task::Poll::Ready(Ok(()))
+    }
+
+    pub fn poll_shutdown(
+        self: std::pin::Pin<&mut Self>,
+        _cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), std::io::Error>> {
+        std::task::Poll::Ready(Ok(()))
+    }
+}
+
+impl Read for Device {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.queue.read(buf)
+    }
+}
+
+impl Write for Device {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.queue.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl D for Device {
+    type Queue = Queue;
+
+    fn name(&self) -> Result<String> {
+        Ok(self.queue.session.get_adapter().get_name()?)
+    }
+
+    fn set_name(&mut self, value: &str) -> Result<()> {
+        self.queue.session.get_adapter().set_name(value)?;
+        Ok(())
+    }
+
+    fn enabled(&mut self, _value: bool) -> Result<()> {
+        Ok(())
+    }
+
+    fn address(&self) -> Result<Ipv4Addr> {
+        let addresses = self.queue.session.get_adapter().get_addresses()?;
+        addresses
+            .iter()
+            .find_map(|a| match a {
+                std::net::IpAddr::V4(a) => Some(*a),
+                _ => None,
+            })
+            .ok_or(Error::InvalidConfig)
+    }
+
+    fn set_address(&mut self, value: Ipv4Addr) -> Result<()> {
+        self.queue.session.get_adapter().set_address(value)?;
+        Ok(())
+    }
+
+    fn destination(&self) -> Result<Ipv4Addr> {
+        // It's just the default gateway in windows.
+        self.queue
+            .session
+            .get_adapter()
+            .get_gateways()?
+            .iter()
+            .find_map(|a| match a {
+                std::net::IpAddr::V4(a) => Some(*a),
+                _ => None,
+            })
+            .ok_or(Error::InvalidConfig)
+    }
+
+    fn set_destination(&mut self, value: Ipv4Addr) -> Result<()> {
+        // It's just set the default gateway in windows.
+        self.queue.session.get_adapter().set_gateway(Some(value))?;
+        Ok(())
+    }
+
+    fn broadcast(&self) -> Result<Ipv4Addr> {
+        Err(Error::NotImplemented)
+    }
+
+    fn set_broadcast(&mut self, value: Ipv4Addr) -> Result<()> {
+        log::debug!("set_broadcast {} is not need", value);
+        Ok(())
+    }
+
+    fn netmask(&self) -> Result<Ipv4Addr> {
+        let current_addr = self.address()?;
+        let netmask = self
+            .queue
+            .session
+            .get_adapter()
+            .get_netmask_of_address(&IpAddr::V4(current_addr))?;
+        match netmask {
+            IpAddr::V4(netmask) => Ok(netmask),
+            _ => Err(Error::InvalidConfig),
+        }
+    }
+
+    fn set_netmask(&mut self, value: Ipv4Addr) -> Result<()> {
+        self.queue.session.get_adapter().set_netmask(value)?;
+        Ok(())
+    }
+
+    fn mtu(&self) -> Result<i32> {
+        Ok(self.mtu as i32)
+    }
+
+    fn set_mtu(&mut self, value: i32) -> Result<()> {
+        self.mtu = value as usize;
+        Ok(())
+    }
+
+    fn queue(&mut self, _index: usize) -> Option<&mut Self::Queue> {
+        Some(&mut self.queue)
+    }
+}
+
+pub struct Queue {
+    session: Arc<Session>,
+    #[cfg(feature = "async")]
+    receiver: tokio::sync::mpsc::UnboundedReceiver<Vec<u8>>,
+    #[cfg(feature = "async")]
+    _task: thread::JoinHandle<()>,
+}
+
+#[cfg(feature = "async")]
+impl Queue {
+    pub fn poll_read(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut [u8],
+    ) -> std::task::Poll<std::io::Result<usize>> {
         match std::task::ready!(self.receiver.poll_recv(cx)) {
             Some(bytes) => {
-                buf.put_slice(&bytes[..]);
-                std::task::Poll::Ready(Ok(()))
+                //dbg!(buf.len(), bytes.len());
+                bytes.iter().enumerate().for_each(|(index, value)| {
+                    buf[index] = *value;
+                });
+                std::task::Poll::Ready(Ok(bytes.len()))
             }
-            None => std::task::Poll::Ready(Ok(())),
+            None => std::task::Poll::Ready(Ok(0)),
         }
     }
 
@@ -118,19 +283,28 @@ impl Device {
     }
 }
 
-impl Read for Device {
+impl Read for Queue {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        match self.receiver.blocking_recv(){
-            Some(pkt) =>{
-                buf.clone_from_slice(&pkt[..]);
-                return Ok(pkt.len())
-            },
-            None => Ok(0),
+        match self.session.receive_blocking() {
+            Ok(pkt) => {
+                let bytes = pkt.bytes();
+                let len = bytes.len();
+                if len <= buf.len() {
+                    buf[..len].clone_from_slice(bytes);
+                    Ok(len)
+                } else {
+                    Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "no large enough storage to save data",
+                    ))
+                }
+            }
+            Err(_) => Err(std::io::Error::new(std::io::ErrorKind::NotConnected, "")),
         }
     }
 }
 
-impl Write for Device {
+impl Write for Queue {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         let len = buf.len();
         let mut write_pack = self.session.allocate_send_packet(len as u16)?;
@@ -144,97 +318,10 @@ impl Write for Device {
     }
 }
 
-impl D for Device {
-    type Queue = Device;
-
-    fn name(&self) -> Result<String> {
-        Ok(self.session.get_adapter().get_name()?)
-    }
-
-    fn set_name(&mut self, value: &str) -> Result<()> {
-        self.session.get_adapter().set_name(value)?;
-        Ok(())
-    }
-
-    fn enabled(&mut self, _value: bool) -> Result<()> {
-        Ok(())
-    }
-
-    fn address(&self) -> Result<Ipv4Addr> {
-        let addresses = self.session.get_adapter().get_addresses()?;
-        addresses
-            .iter()
-            .find_map(|a| match a {
-                std::net::IpAddr::V4(a) => Some(*a),
-                _ => None,
-            })
-            .ok_or(Error::InvalidConfig)
-    }
-
-    fn set_address(&mut self, value: Ipv4Addr) -> Result<()> {
-        self.session.get_adapter().set_address(value)?;
-        Ok(())
-    }
-
-    fn destination(&self) -> Result<Ipv4Addr> {
-        // It's just the default gateway in windows.
-        self.session
-            .get_adapter()
-            .get_gateways()?
-            .iter()
-            .find_map(|a| match a {
-                std::net::IpAddr::V4(a) => Some(*a),
-                _ => None,
-            })
-            .ok_or(Error::InvalidConfig)
-    }
-
-    fn set_destination(&mut self, value: Ipv4Addr) -> Result<()> {
-        // It's just set the default gateway in windows.
-        self.session.get_adapter().set_gateway(Some(value))?;
-        Ok(())
-    }
-
-    fn broadcast(&self) -> Result<Ipv4Addr> {
-        Err(Error::NotImplemented)
-    }
-
-    fn set_broadcast(&mut self, value: Ipv4Addr) -> Result<()> {
-        log::debug!("set_broadcast {} is not need", value);
-        Ok(())
-    }
-
-    fn netmask(&self) -> Result<Ipv4Addr> {
-        let current_addr = self.address()?;
-        let netmask = self
-            .session
-            .get_adapter()
-            .get_netmask_of_address(&IpAddr::V4(current_addr))?;
-        match netmask {
-            IpAddr::V4(netmask) => Ok(netmask),
-            _ => Err(Error::InvalidConfig),
+impl Drop for Queue {
+    fn drop(&mut self) {
+        if let Err(err) = self.session.shutdown() {
+            log::error!("failed to shutdown session: {:?}", err);
         }
     }
-
-    fn set_netmask(&mut self, value: Ipv4Addr) -> Result<()> {
-        self.session.get_adapter().set_netmask(value)?;
-        Ok(())
-    }
-
-    fn mtu(&self) -> Result<i32> {
-        Ok(self.mtu as i32)
-    }
-
-    fn set_mtu(&mut self, value: i32) -> Result<()> {
-        self.mtu = value as usize;
-        Ok(())
-    }
-
-    fn queue(&mut self, _index: usize) -> Option<&mut Self::Queue> {
-        Some(self)
-    }
 }
-
-pub struct Queue;
-
-
